2a550cb92004149a41dd09840bafe975a83116f5
Soul <rg8947930@gmail.com>
2026-03-09 15:50:21 +0530

    day 4

---
+++ lecture4.js
@@ -0,0 +1,30 @@
+// var myinfo = new Array();
+// var myinfo1 = Array();
+// console.log(myinfo1); // console.log(myinfo); they both create a array.
+
+
+
+
+// let collection=[{}, [], true, "john", function () {}, 9900112233, undefined, null, new String("abc"), new Date()];
+// console.log(collection);
+// collection.teacherName= "John Mac";     // here indexing not increases because they are not added by indexing
+// collection.teacherName= "John Mac";     // here indexing not increases because they are not added by indexing
+// collection.phone=1234567890;
+// console.log(collection);
+
+
+
+// let trainer=["jintendar", 29, 12345678, {isMarried: true}];
+// console.log(trainer);
+// console.log(trainer[trainer.length-1])
+
+
+
+// const result=document.getElementById("para");
+// console.log(result);
+// result.innerHTML="my self";
+
+
+const result=document.getElementsByClassName("myClass");
+result.innerHTML="hii";
+console.log(result);
